f2649462e9028fb9ab8e8a9ad2c9d25bae3c70ad
willyouavocuddleme <hardtodie1999@gmail.com>
2021-08-18 11:15:02 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: ' mừng ngày sinh nhật bé iuu ヾ(⌐■_■)ノ♪',
+    introDesc: 'sit nicely để ba lun tặng quà cho con nèo',
+    btnIntro: 'daddy daddy cool',
+    title: 'quà của ai đây? quà của ai nhờ?',
+    desc: 'chọn cho chuẩn zô, không chọn là chó',
+    btnYes: 'BỨU BỪU BA LUN',
+    btnNo: 'IPAD PRO M1 512GB',
+    question:'quàooo (⁄ ⁄>⁄ ▽ ⁄<⁄ ⁄) sao lại thích bứu bừu ba lun ztr?',
+    btnReply: 'yêu nhắm ♡ (￣З￣)',
+    reply: 'CHYM TOヽ(°〇°)ﾉ QUÁ TRỜI',
+    mess: 'mê ( • )( • )ԅ(≖‿≖ԅ) lun',
+    messDesc: '❤ giỏi nhắm, giờ là món quà thứ 2 cho bé nè, zô chọn mẫu ik (ɔˆз(ˆ⌣ˆc)',
+    btnAccept: 'IPAD PRO M1 512GB',
+    messLink: 'https://vt.tiktok.com/ZSJWPkQfT/'
+}
